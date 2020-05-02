2dd141f083eb0480564e6160229cf192fed9e23f
Firbath <Bc19851128>
2020-05-02 15:44:28 +0800

    change to sqlite

--- flask_service/src/service/module/database.py
+++ flask_service/src/service/module/database.py
@@ -14,11 +14,8 @@ def init_app(app):
     # 配置
     config = app.default_config
     # 数据库
-    db_host = config.get('mysql', 'MYSQL_HOST')
-    db_user = config.get('mysql', 'MYSQL_USER')
-    db_pwd = config.get('mysql', 'MYSQL_PWD')
-    db_name = config.get('mysql', 'DB_NAME')
-    db_uri = 'mysql+pymysql://' + db_user + ':' + db_pwd + '@' + db_host + '/' + db_name + '?charset=utf8'
+    db_info = config.get('data', 'sqlite')
+    db_uri = 'sqlite:///%s?charset=utf8' % db_info
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_POOL_RECYCLE'] = 80
